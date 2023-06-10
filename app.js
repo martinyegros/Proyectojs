d8a37be098940803d7caf2cd1c0d7fbcf43cd547
martinyegros <martinyegros1@gmail.com>
2023-06-10 19:18:51 -0300

    Funcionalidad del carrito

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const items = document.getElementById('items')
 const templateCard = document.getElementById('template-card').content
 const fragment = document.createDocumentFragment()
+const botonCart = document.querySelector('.container-cart-icon');
+const containerCartProducts = document.querySelector('.container-cart-products');
+const cartInfo = document.querySelector('.cart-product');
+const rowProduct = document.querySelector('.row-product');
+const productsList = document.querySelector('.row');
+const cartEmpty = document.querySelector('.cart-empty');
+const cartTotal = document.querySelector('.cart-total');
+const valorTotal = document.querySelector('.total-pagar');
+const countProducts = document.querySelector('#contador-productos');
+const pagar = document.querySelector('.cart-pagar');
+let carrito = {};
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
+    if (localStorage.getItem('carrito')) {
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        showHTML()
+    }
 })
+items.addEventListener('click', e => {
+    addCarrito(e)
+})
+
+rowProduct.addEventListener('click', e => {
+    btnAccion(e)
+})
+
+rowProduct.addEventListener('click', e => {
+    btnEliminar(e)
+})
+
 
 const fetchData = async () => {
     try {
@@ -29,101 +56,41 @@ const pintarCards = data => {
     items.appendChild(fragment)
 }
 
-
-
-
-/*
-const setCarrito = objeto => {
-    const producto = {
-        id: objeto.querySelector('.btn-dark').dataset.id,
-        nombre: objeto.querySelector('h5').textContent,
-        precio: objeto.querySelector('p').textContent,
-        cantidad: 1
-    }
-
-
-if (carrito.hasOwnProperty(producto.id)) {
-    producto.cantidad = carrito[producto.id].cantidad + 1
-}
-
-    carrito[producto.id] = {...producto}
-    pintarCarrito()
-}
- */
-
-
-
-
-
-
-//////////////////
-const botonCart = document.querySelector('.container-cart-icon');
-const containerCartProducts = document.querySelector('.container-cart-products');
-
 botonCart.addEventListener('click', () => {
     containerCartProducts.classList.toggle('hidden-cart');
 });
 
-
-const cartInfo = document.querySelector('.cart-product');
-const rowProduct = document.querySelector('.row-product');
-///////////
-const productsList = document.querySelector('.row');
-//////////
-let allProducts = [];
-
-const valorTotal = document.querySelector('.total-pagar');
-const countProducts = document.querySelector('#contador-productos');
-
-const cartEmpty = document.querySelector('.cart-empty');
-const cartTotal = document.querySelector('.cart-total');
-
-/////////
-productsList.addEventListener('click', e => {
-    if(e.target.classList.contains('btn-dark')) {
-        const producto = e.target.parentElement;
-
-        const infoProduct = {
-            quantity: 1,
-            nombre: producto.querySelector('h5').textContent,
-            precio: producto.querySelector('span').textContent,
-        };
-        
-        const exits = allProducts.some(producto => producto.nombre === infoProduct.nombre);
-
-        if (exits) {
-            const products = allProducts.map(producto => {
-                if(producto.nombre === infoProduct.nombre){
-                    producto.quantity++;
-                    return producto;
-                } else {
-                    return producto;
-                }
-            });
-            
-            allProducts = [...products];
-        } else {
-            allProducts = [...allProducts, infoProduct];
-        }
-
-        showHTML();
-    } 
-});
-
-rowProduct.addEventListener('click', e => {
-    if(e.target.classList.contains('icon-close')){
-        const producto = e.target.parentElement;
-        const title = producto.querySelector('.btn-dark');
-
-        allProducts = allProducts.filter( producto => producto.id !== title);
-
-        showHTML();
+const addCarrito = e => {
+    if (e.target.classList.contains('btn-dark')) {
+        setCarrito(e.target.parentElement)
     }
+    e.stopPropagation()
+}
+
+pagar.addEventListener('click', e => {
+    btnPagar(e)
 })
 
-const showHTML = () => {
+const setCarrito = objeto => {
+    const producto = {
+        cantidad: 1,
+        id: objeto.querySelector('.btn-dark').dataset.id,
+        nombre: objeto.querySelector('h5').textContent,
+        precio: objeto.querySelector('span').textContent,
+    }
 
-    if(allProducts.lenght){
+    if(carrito.hasOwnProperty(producto.id)) {
+        producto.cantidad = carrito[producto.id].cantidad + 1
+    }
+
+    carrito[producto.id] = {...producto}
+
+    showHTML();
+}
+
+const showHTML = () => {
+    
+    if(carrito.lenght){
         cartEmpty.classList.remove('hidden');
 		rowProduct.classList.add('hidden');
 		cartTotal.classList.add('hidden');
@@ -138,29 +105,110 @@ const showHTML = () => {
     let total = 0;
     let totalOfProducts = 0;
 
-    allProducts.forEach(producto => {
+    Object.values(carrito).forEach(producto => {
         const containerProduct = document.createElement('div');
         containerProduct.classList.add('cart-product');
 
         containerProduct.innerHTML = `
         <div class="info-cart-product">
-            <span class="cantidad-producto-carrito">${producto.quantity}</span>
-            <p class="titulo-producto-carrito">${producto.nombre}</p>
-            <span class="precio-producto-carrito">${producto.precio}</span>
-        </div>
-        <div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="icon-close">
-                <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clip-rule="evenodd" />
+            <div class="d-titc">
+                <p class="titulo-producto-carrito">${producto.nombre}</p>
+            </div>
+            <div class="d-cant">
+                <span class="cantidad-producto-carrito" >${producto.cantidad}</span>
+            </div>
+            <div class="d-botones">
+                <button class="btn btn-info btn-sm" id="${producto.id}">+</button>
+                <button class="btn btn-danger btn-sm" id="${producto.id}">-</button>
+            </div>
+            <div class="d-prec">
+                <span class="precio-producto-carrito">${producto.precio}</span>
+            </div>
+            <div class="d-elim">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-close" id="${producto.id}">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
             </svg>
+            </div>
         </div>
         `;
-
         rowProduct.append(containerProduct);
 
-        total = total + parseInt(producto.quantity * producto.precio.slice(1));
-        totalOfProducts = totalOfProducts + producto.quantity;
+        total = total + parseInt(producto.cantidad * producto.precio.slice(1));
+        totalOfProducts = totalOfProducts + producto.cantidad;
+
     });
+
 
     valorTotal.innerText = `$${total}`;
     countProducts.innerText = totalOfProducts;
+
+    localStorage.setItem('carrito', JSON.stringify(carrito))
 }
+
+
+const btnAccion = e => {
+    if(e.target.classList.contains('btn-info')) {
+        const producto = carrito[e.target.id]
+        producto.cantidad++
+        carrito[e.target.id] = {...producto}
+        showHTML();
+    }
+
+    if(e.target.classList.contains('btn-danger')) {
+        const producto = carrito[e.target.id]
+        producto.cantidad--
+        if(producto.cantidad === 0) {
+            delete carrito[e.target.id];
+        } 
+        showHTML();
+    }
+
+    e.stopPropagation()
+}
+
+const btnEliminar = e => {
+    if(e.target.classList.contains('icon-close')) {
+        Swal.fire({
+            title: 'Estas seguro?', 
+            icon: 'warning',
+            showCancelButton: true,
+            cancelButtonText: 'No',
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                delete carrito[e.target.id];
+                showHTML();
+                Swal.fire(
+                'Eliminado!',
+                'La fila ha sido eliminada',
+                'success'
+                )
+            }
+        })
+    }
+
+    e.stopPropagation()
+}
+
+const btnPagar = e => {
+    if (e.target.classList.contains('btn-pagar')) {
+        if(valorTotal.innerText != '$0') {
+            Swal.fire(
+                'Compra Exitosa',
+                'Gracias por su compra',
+                'success'
+            )
+            carrito = {};
+            showHTML();
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'No hay produto!',
+                text: 'Carrito vacío!',
+                })
+            }
+        }
+        e.stopPropagation()
+    }
